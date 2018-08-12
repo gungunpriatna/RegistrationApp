fb6116c4eaee4c451a6fb654693ba5e318edd70c
Sonya Luvita <gungunpriatna02@gmail.com>
2018-08-12 11:43:11 +0700

    insert data to database

---
+++ app/src/main/java/me/gungunpriatna/registrationapp/RegisterAPI.java
@@ -0,0 +1,16 @@
+package me.gungunpriatna.registrationapp;
+
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
+public interface RegisterAPI {
+
+    @FormUrlEncoded
+    @POST("insert.php")
+    Call<Value> daftar(@Field("npm") String npm,
+                       @Field("nama") String nama,
+                       @Field("kelas") String kelas,
+                       @Field("sesi") String sesi);
+}
---
+++ app/src/main/java/me/gungunpriatna/registrationapp/Value.java
@@ -0,0 +1,15 @@
+package me.gungunpriatna.registrationapp;
+
+public class Value {
+
+    String value;
+    String message;
+
+    public String getValue() {
+        return value;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
